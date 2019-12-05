8a3617aeb4d8f7e21c76e7f17f861518a0fed3f5
hugodecasta <hugodecasta@orange.fr>
2019-12-05 12:16:40 +0100

    allow prefix data for each key

--- boolMaster.js
+++ boolMaster.js
@@ -6,16 +6,27 @@ class BoolMaster {
 
     constructor(host) {
         this.host = host
-        this.prefix = ''
+        this.prefix_data = {}
         this.checkers = {}
         this.checker_id = {}
     }
 
     // -----------------------------------------
 
-    async send(method, kwargs, prefix=null) {
-        prefix = prefix==null?this.prefix:prefix
-        kwargs['key'] = prefix+kwargs['key']
+    async send(method, kwargs, prefix_data=null) {
+
+        prefix_data = prefix_data==null?this.prefix_data:prefix_data
+        let key = kwargs.key
+
+        let prefix = ''
+
+        if(prefix_data.hasOwnProperty(key))
+            prefix = prefix_data[key]
+        else if (prefix_data.hasOwnProperty('*'))
+            prefix = prefix_data['*']
+
+        kwargs['key'] = prefix+key
+
         let url = this.host+'/?method='+method
         for(let arg in kwargs) {
             let value = kwargs[arg]
@@ -28,43 +39,48 @@ class BoolMaster {
         })
     }
 
-    set_prefix(prefix) {
-        this.prefix = prefix
+    reset_prefix() {
+        this.prefix_data = {}
     }
 
-    get_prefix() {
-        return this.prefix
+    set_prefix(prefix, key='*') {
+        this.prefix_data[key] = prefix
+    }
+
+    get_prefix(key='*') {
+        return this.prefix_data[key]
     }
 
     // -----------------------------------------
 
-    async read_key(key,prefix=null) {
-        return await this.send('read_key',{key:key}, prefix)
+    async read_key(key,prefix_data=null) {
+        return await this.send('read_key',{key:key}, prefix_data)
     }
 
-    async key_exists(key,prefix=null) {
-        return await this.send('key_exists', {key:key}, prefix)
+    async key_exists(key,prefix_data=null) {
+        return await this.send('key_exists', {key:key}, prefix_data)
     }
 
-    async key_remove(key,prefix=null) {
-        return await this.send('key_remove', {key:key}, prefix)
+    async key_remove(key,prefix_data=null) {
+        return await this.send('key_remove', {key:key}, prefix_data)
     }
 
-    async write_key(key, file_data,prefix=null) {
+    async write_key(key, file_data,prefix_data=null) {
         file_data = JSON.stringify(file_data)
-        return await this.send('write_key', {key:key, file_data:file_data}, prefix)
+        return await this.send('write_key', {key:key, file_data:file_data}, prefix_data)
     }
 
     // -----------------------------------------
 
     create_key_checker(key) {
-        let saved_prefix = this.prefix
         this.checkers[key] = {
             int:null,
+            saved_prefix_data:this.prefix_data,
             int_method:async function() {
-                if(! await tthis.key_exists(key,saved_prefix))
+                let saved_prefix_data = tthis.checkers[key].saved_prefix_data
+                if(! await tthis.key_exists(key,saved_prefix_data))
                     return
-                let data = await tthis.read_key(key,saved_prefix)
+                let data = await tthis.read_key(key,saved_prefix_data)
                 let check_data = JSON.stringify(data)
                 let last_data = tthis.checkers[key].last_data
                 if(check_data != last_data) {
@@ -115,6 +131,7 @@ class BoolMaster {
         delete this.checker_id[id]
         if(Object.keys(this.checkers[key].callbacks) == 0) {
             clearInterval(this.checkers[key].int)
+            console.log('remove checker',key,this.checkers[key])
             delete this.checkers[key]
         }
     }
